154ea69155335b44ceaf8355fb3d6bb14fa021c9
Jim <jimmy.the.walk@gmail.com>
2024-06-08 19:42:46 -0400

    update env type and prisma

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -7,12 +7,12 @@ import prisma from "../../../../prisma/lib/prisma"
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         }),
         GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string
         })
     ],
     adapter: PrismaAdapter(prisma),
