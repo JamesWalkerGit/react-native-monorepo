f2ef212cfdac300a1f1ec33003198b2752d66a64
James Walker <jimmy.the.walk@gmail.com>
2024-06-19 19:31:45 -0400

    re-added github oauth with auth.js, added tests and mocks for auth and router (#8)

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,41 +5,74 @@ import Confetti from 'react-confetti'
 import { useState } from "react";
 import Image from 'next/image';
 import { StyleSheet } from "../styles/Stylesheet";
-import { Button } from "@nextui-org/react";
+import { Button, Spinner } from "@nextui-org/react";
+import { signIn, signOut, useSession } from "next-auth/react"
+import { IconBrandGithub } from "@tabler/icons-react";
 
 
 export default function Web() {
   const [confettiStatus, setConfettiStatus] = useState(false);
 
   const router = useRouter();
+  const session = useSession();
+
   const goTo = (route: string) => {
     router.push('/' + route)
   }
-
 
   const toggleConfetti = () => {
     setConfettiStatus(!confettiStatus)
   }
 
   return (
-    <>
-      <title>Jprojects</title>
-      <div style={styles.container}>
-        {
-          confettiStatus ?
-            <Confetti />
-            : null
-        }
-        <>
-          <div style={styles.partyContainer}>
-            <Image src={"/static/images/parrot.gif"} alt={"partyParrot"} width={200} height={200} style={{ padding: 10 }}></Image>
-            <Button color="secondary" onPress={toggleConfetti} style={styles.partyButton}>
-              Party Button 🎉
-            </Button>
-          </div>
-        </>
-      </div >
-    </>
+
+    session.status === 'loading' ?
+      <>
+        <div style={styles.loadingContainer}>
+          <Spinner aria-label="loading-spinner" />
+        </div>
+      </>
+      :
+      <>
+        <title>Jprojects</title>
+        <div style={styles.container}>
+          {
+            confettiStatus ?
+              <Confetti aria-label="confetti-party" />
+              : null
+          }
+          <>
+            <div style={styles.partyContainer}>
+              <Image priority={true} src={"/static/images/parrot.gif"} alt={"partyParrot"} width={200} height={200} style={{ padding: 10 }}></Image>
+              <Button color="secondary" onPress={toggleConfetti} style={styles.partyButton}>
+                Party Button 🎉
+              </Button>
+            </div>
+            <div style={styles.partyContainer}>
+              {session.status === 'unauthenticated' ?
+                <>
+                  <Button onPress={() => signIn()} style={styles.githubButton} startContent={<IconBrandGithub color="white" />}>
+                    Sign In With GitHub
+                  </Button>
+                </>
+                : session.status === 'authenticated' ?
+                  <>
+                    {session.data.user?.email ?
+                      <>
+                        <div style={styles.loggedInContainer}>Logged in as {session.data.user.email}</div>
+                      </>
+                      : null
+                    }
+                    <Button color="secondary" onPress={() => signOut()} style={styles.githubButton}>
+                      Sign Out
+                    </Button>
+                  </>
+                  : null
+              }
+            </div>
+          </>
+        </div >
+      </>
   );
 }
 
@@ -51,17 +84,10 @@ const styles = StyleSheet.create({
   partyContainer: {
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1,
+    flex: .5,
     display: 'flex',
     flexDirection: 'column',
-    marginBottom: 100
-  },
-  authContainer: {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    marginTop: 'auto',
-    maxWidth: 150,
+    height: '50%'
   },
   loadingContainer: {
     justifyContent: 'center',
@@ -71,8 +97,12 @@ const styles = StyleSheet.create({
     height: '100%'
   },
   partyButton: {
-    height: 90,
-    width: 180,
     fontSize: 22,
+  },
+  githubButton: {
+    backgroundColor: '#161b22'
+  },
+  loggedInContainer: {
+    padding: 20
   }
 });
--- apps/web/app/providers/Providers.tsx
+++ apps/web/app/providers/Providers.tsx
@@ -1,11 +1,14 @@
 // app/providers.tsx
 
 import { NextUIProvider } from '@nextui-org/react'
+import { SessionProvider } from 'next-auth/react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
+    <SessionProvider>
       <NextUIProvider>
         {children}
       </NextUIProvider>
+    </SessionProvider>
   )
 }
