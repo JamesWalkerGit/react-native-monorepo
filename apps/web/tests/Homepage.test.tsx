bed0e6df03225eb6ff613cadce5fb90575a53bf6
James Walker <jamesaw2324@gmail.com>
2024-06-19 16:17:01 -0400

    fix test

--- apps/web/tests/Homepage.test.tsx
+++ apps/web/tests/Homepage.test.tsx
@@ -6,7 +6,7 @@ describe('Page', () => {
     it('renders a heading', () => {
         render(<Web />)
 
-        const heading = screen.getByRole('button', { name: 'Party Button 🎉 --BROKEN FAIL' })
+        const heading = screen.getByRole('button', { name: 'Party Button 🎉' })
 
         expect(heading).toBeInTheDocument()
     })
