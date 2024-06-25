c5f76c87bbd9836d7b6acf0f31b6277d128b35c4
James Walker <jamesaw2324@gmail.com>
2024-06-24 22:00:26 -0400

    testing event types

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -43,7 +43,7 @@ export default function Homepage() {
                 Party Button 🎉
               </Button>
               <Modal opened={opened} onClose={close} title="Congratulations!">
-                You did it! 🥳  -- It really works 😤
+                You did it! 🥳  -- testing event types
               </Modal>
               <Button variant='gradient' style={styles.modalButton} onClick={open}>Click it? 👀</Button>
             </div>
