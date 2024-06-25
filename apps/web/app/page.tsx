a6e649b45e489952907d56c22f720b3db36c3b26
James Walker <jamesaw2324@gmail.com>
2024-06-24 22:24:54 -0400

    test envs

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -43,7 +43,7 @@ export default function Homepage() {
                 Party Button 🎉
               </Button>
               <Modal opened={opened} onClose={close} title="Congratulations!">
-                You did it! 🥳  -- testing with action
+                You did it! 🥳  -- testing pull request name
               </Modal>
               <Button variant='gradient' style={styles.modalButton} onClick={open}>Click it? 👀</Button>
             </div>
