902654c706a82874e931f357ce1cae7367f5d27c
James Walker <jimmy.the.walk@gmail.com>
2024-06-27 23:58:43 -0400

    Add msclarity script (#32)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -7,6 +7,7 @@ import { ColorSchemeScript } from "@mantine/core";
 import '@mantine/core/styles.css';
 import Navbar from "./components/Navbar";
 import { Metadata } from 'next'
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: 'Jprojects',
@@ -18,6 +19,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <>
       <html lang="en" style={styles.html}>
         <head>
+          <Script strategy="lazyOnload" id="clarity-script">
+            {`
+              (function (c, l, a, r, i, t, y) {
+                c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+                t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+                y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+              })(window, document, "clarity", "script", "mysc6s9qv1");
+            `}
+          </Script>
           <ColorSchemeScript defaultColorScheme='dark' />
         </head>
         <body style={styles.body}>
