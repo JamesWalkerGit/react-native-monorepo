426c984f239d4c4d35663bf6b32e76fcb73398a0
James Walker <jimmy.the.walk@gmail.com>
2024-06-28 03:12:50 -0400

    added secret to config (#38)

--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -4,4 +4,5 @@ import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [GitHub],
+    secret: 'yzucNcSAAfVNi0SvvbSQ1lOTYNjIJPDGT2BE/8T+ax8='
 })
