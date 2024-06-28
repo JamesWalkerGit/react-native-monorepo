dec3842939c7d4d70d5daa6986dc0cc6ece246a8
James Walker <jimmy.the.walk@gmail.com>
2024-06-28 04:05:27 -0400

    remove secret

--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -3,6 +3,5 @@ import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [GitHub],
-    secret: 'yzucNcSAAfVNi0SvvbSQ1lOTYNjIJPDGT2BE/8T+ax8='
+    providers: [GitHub]
 })
