ca4da09614a25f50e796836f5b04bb80900a68b7
James Walker <jimmy.the.walk@gmail.com>
2024-06-28 00:25:59 -0400

    added msClarity package

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -7,26 +7,19 @@ import { ColorSchemeScript } from "@mantine/core";
 import '@mantine/core/styles.css';
 import Navbar from "./components/Navbar";
 import { Metadata } from 'next'
-import Script from "next/script";
+import { initialize } from 'react-native-clarity';
+
 
 export const metadata: Metadata = {
   title: 'Jprojects',
   description: 'Jprojects site collection',
 }
+initialize("mysc6s9qv1");
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <html lang="en" style={styles.html}>
-        <Script strategy="lazyOnload" id="clarity-script">
-          {`
-              (function (c, l, a, r, i, t, y) {
-                c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-                t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
-                y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-              })(window, document, "clarity", "script", "mysc6s9qv1");
-            `}
-        </Script>
         <head>
           <ColorSchemeScript defaultColorScheme='dark' />
         </head>
