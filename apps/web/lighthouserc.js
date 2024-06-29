ca060c1209ec907531900ff60b7f9fabab31797c
James Walker <jimmy.the.walk@gmail.com>
2024-06-28 23:57:14 -0400

    update to warn for production

--- apps/web/lighthouserc.js
+++ apps/web/lighthouserc.js
@@ -7,9 +7,9 @@ module.exports = {
         assert: {
             assertions: {
                 "categories:performance": ["warn", { "minScore": .85 }],
-                "categories:accessibility": ["error", { "minScore": 1 }],
-                "categories:bestPractices": ["error", { "minScore": 1 }],
-                "categories:seo": ["error", { "minScore": 1 }],
+                "categories:accessibility": ["warn", { "minScore": 1 }],
+                "categories:bestPractices": ["warn", { "minScore": 1 }],
+                "categories:seo": ["warn", { "minScore": 1 }],
             },
         },
     },
