d5f01970a3a13130a9c4771a68879a6be936de7c
James Walker <jimmy.the.walk@gmail.com>
2025-03-17 22:21:30 -0400

    test loading

--- apps/web/app/contact/page.tsx
+++ apps/web/app/contact/page.tsx
@@ -41,7 +41,7 @@ export default function Contact() {
                             <Text style={styles.contactText}>{text}</Text>
                         </div>
                         <div style={styles.contactIconContainer}>
-                            <Image src={image} alt={imageAlt} priority={true}></Image>
+                            <Image src={image} alt={imageAlt} priority={true} loading='eager'></Image>
                         </div>
                     </div>
                 </Link>
