0e8b9dac78af6c3b34b1f981f11df32f966ba2d0
James Walker <jimmy.the.walk@gmail.com>
2026-03-23 00:13:02 -0400

    update dependencies (#54)

--- apps/web/tests/Homepage.test.tsx
+++ apps/web/tests/Homepage.test.tsx
@@ -92,8 +92,13 @@ describe('Homepage', () => {
             partyButton.click();
         });
 
-        const expectedFadeStyles = "z-index: 2; position: absolute; pointer-events: none; top: 0px; left: 0px; bottom: 0px; right: 0px; transition-property: opacity, transform; transition-duration: 400ms; transition-timing-function: ease; opacity: 0; transform: translateY(calc(-1.875rem * var(--mantine-scale));"
         const confettiAfter = screen.queryByLabelText('confetti-party');
-        expect(confettiAfter).toHaveAttribute('style', expectedFadeStyles);
+        expect(confettiAfter).toHaveStyle({
+            opacity: '0',
+            position: 'absolute',
+            pointerEvents: 'none'
+        });
+        expect(confettiAfter?.getAttribute('style')).toContain('transition-property: opacity, transform');
+        expect(confettiAfter?.getAttribute('style')).toContain('transform: translateY(');
     })
 })
