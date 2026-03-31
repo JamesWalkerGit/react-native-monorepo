e27e75f152b94b83a2d2b298646e0532d44a0926
James Walker <jimmy.the.walk@gmail.com>
2026-03-31 01:26:13 -0400

    update footer test for resume

--- apps/web/tests/Footer.test.tsx
+++ apps/web/tests/Footer.test.tsx
@@ -5,12 +5,12 @@ import Footer from '@/app/components/Footer/Footer';
 
 
 describe('Footer', () => {
-    it('has GithubButton and Contact button', async () => {
+    it('has GithubButton and Resume button', async () => {
 
         render(<Footer />);
 
-        const contactButton = await screen.findByRole('button', { name: 'Contact' });
-        expect(contactButton).toBeInTheDocument();
+        const resumeButton = await screen.findByRole('button', { name: 'Resume' });
+        expect(resumeButton).toBeInTheDocument();
 
         const githubLink = screen.getByRole('link', { name: 'Github Icon' });
         expect(githubLink).toHaveAttribute('href', 'https://github.com/JamesWalkerGit/react-native-monorepo/tree/main/apps/web');
